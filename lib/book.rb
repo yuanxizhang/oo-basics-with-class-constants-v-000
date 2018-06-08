057a6747513596d0787792484bf3c77af96471b1
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-08 21:16:23 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,8 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count 
+  attr_reader :title, :genre
+  
+  GENRES = ["Thriller", "Science Fiction", "Romance"]
 
   def initialize(title)
     @title = title
@@ -9,5 +11,11 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
+  
+  def genre=(new_genre)
+    @genre = new_genre
+    unless GENRES.include?(new_genre)
+      GENRES << new_genre
+    end
+  end
 end
