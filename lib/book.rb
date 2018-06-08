73ee63767866fd7f5ee06d50ea335daf3e52f9c9
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-08 21:11:51 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ class Book
   attr_accessor :author, :page_count 
   attr_reader :title, :genre
   
-  GENRES = 
+  GENRES = ["Thriller", "Science Fiction", "Romance"]
 
   def initialize(title)
     @title = title
